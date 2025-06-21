0439424943fcd844d276c59d2575ce0ccc3ce2ee
hunterckm01 <huner6894@gmail.com>
2025-06-21 16:36:22 +0530

    new changes made of deleting

--- src/components/core/HomePage/LearningLanguageSection.jsx
+++ src/components/core/HomePage/LearningLanguageSection.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import HighlightText from './HighlightText'
-import know_your_progress from '../../../assets/Images/know_your_progress.png'
-import compare_with_others from "../../../assets/Images/Compare_with_others.png";
-import plan_your_lesson from "../../../assets/Images/Plan_your_lessons.png";
+import know_your_progress from '../../../assets/Images/know_your_progress.svg'
+import compare_with_others from "../../../assets/Images/Compare_with_others.svg";
+import plan_your_lesson from "../../../assets/Images/Plan_your_lessons.svg";
 import CTAButton from './Button';
 
 const LearningLanguageSection = () => {
